31a7b9e34f25f9948f9ce5fd23f570004a1ba052
Francisco Cordero Ronquillo <81389205+FranceSheep@users.noreply.github.com>
2024-02-04 07:14:44 -0600

    Update lib.rs

--- Contracts/AsynchronousContractTemplate/io/src/lib.rs
+++ Contracts/AsynchronousContractTemplate/io/src/lib.rs
@@ -9,43 +9,32 @@ use gmeta::{In,Out,InOut,Metadata};
 pub enum Action {
     
     // Add Actions
-    FirstAction, // Example an action with a simple input
-    SecondAction(String), // Example an action with a u128 input
-    ThirdAction(u128), // Example an action with a String input
-    Fourthaction(CustomInput), // Example an action with a custom input
-  
+    Overtaking(String), // Example an action with a simple input
+    Pay(u128), // Example an action with a u128 input
+    Match_pay(u128)
     
 }
-
-// Example of a custom input for an action
-#[derive(Default, Encode, Decode, Clone, TypeInfo)]
-pub struct CustomInput {
-    firstfield: String,
-    secondfield: u128,
-    thirdfield: ActorId,
-   
-}
-
 
 // 2. Create your own Events
 #[derive(Encode, Decode, TypeInfo, Hash, PartialEq, PartialOrd, Eq, Ord, Clone, Copy, Debug)]
 pub enum  Event {
     
     // Add Events(Example)
-    FirstEvent,
-    SecondEvent,
-    ThirdEvent,
-    FourtEvent
+    TimeoutEscrow,
+    Pay,
+    Finish,
+    Overtake,
+    Win,
+    Loss
 }
 
 
 // 3. Create your own Struct
 #[derive(Default, Clone, Encode, Decode, TypeInfo)]
-pub struct IoCustomStruct {
-   pub firstfield: String,
-   pub secondfield: u128,
-   pub thirdfield: Vec<(ActorId,u128)>
-   
+pub struct Leaderboard {
+   pub name_field: String,
+   pub id_field: ActorId,
+   pub points_field: u128
 }
 
 // 4. Create your init Struct
@@ -64,10 +53,8 @@ pub struct ContractMetadata;
 // 5. Define the structure of actions, events and state for your metadata.
 impl Metadata for ContractMetadata{
      type Init = In<InitStruct>;
-     type Handle = InOut<Action,Event>;
-     type Others = ();
-     type Reply=();
-     type Signal = ();
+     type Surpass = InOut<Action,Event>;
+     type Top = ();
      type State = Out<IoCustomStruct>;
 
 }
